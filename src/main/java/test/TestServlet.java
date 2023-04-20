7ee11000b01ababa7dc6e00290d3dece18b389e9
unnamed <x1593572582004@yahoo.com.tw>
2023-04-20 11:44:59 +0800

    第一次上傳

---
+++ src/main/java/test/TestServlet.java
@@ -0,0 +1,24 @@
+package test;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/TestServlet")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("測試試  test");
+	}
+
+
+
+}
